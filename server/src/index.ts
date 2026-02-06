8551ffe5d9ad3cd2a9caa1ddb5836c84db250e11
Pratham C <chauhanpratham767@gmail.com>
2026-02-06 18:24:49 +0530

    feat:Server file

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const app = express();
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
