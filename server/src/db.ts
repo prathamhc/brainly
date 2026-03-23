b0a8abf560ee057ca05e44548813d09ee888c20c
Pratham C <chauhanpratham767@gmail.com>
2026-03-23 16:33:59 +0530

    add:Content Schema

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,5 +1,6 @@
 import mongoose, { model, Schema } from "mongoose";
 import { MONGO_URL } from "./config.js";
+import { required } from "zod/mini";
 
 mongoose.connect(MONGO_URL);
 
@@ -8,4 +9,12 @@ const UserSchema = new Schema({
     password: String,
 })
 
-export const UserModel = model("User", UserSchema); 
+export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
+    userId: { type: mongoose.Types.ObjectId, ref: "User", required: true }
+})
+export const ContentModel = model("Content", ContentSchema);
